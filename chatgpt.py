7411e265ac93227ab1752f0ec4f42a8438cdfdd0
GHF <gustavohf099@gmail.com>
2023-10-05 18:59:07 -0300

    Alteração na estrutura de chamada

--- chatgpt.py
+++ chatgpt.py
@@ -5,8 +5,8 @@ openai.api_key = ''
 def gerar_mensagem_chatgpt():
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt="Gerar mensagem para status financeiro:",  # Prompt para o ChatGPT
-        max_tokens=50 
+        prompt="Gerar mensagem para status financeiro:", 
+        max_tokens=10 
     )
     
     mensagem = response.choices[0].text.strip()
